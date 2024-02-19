36b60fdc61d79512428de3c5eea994c591c2256d
Asandei Stefan <asandei.stefanel@gmail.com>
2024-02-19 16:53:25 +0200

    search for food products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 import { getServerAuthSession } from "@/server/auth";
 import LandingPage from "@/components/screens/landing-page";
 import { api } from "@/trpc/server";
-import FoodCard from "@/components/food-cart";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { Icons } from "@/components/icons";
+import ProductCatalog from "@/components/screens/product-catalog";
 
 export default async function IndexPage() {
   const session = await getServerAuthSession();
@@ -12,27 +9,5 @@ export default async function IndexPage() {
 
   const food = await api.admin.getFoodProducts.query();
 
-  return (
-    <section className="container grid items-center gap-6 pb-8 pt-3">
-      <div className="flex w-full flex-col items-center justify-between gap-2 sm:flex-row">
-        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
-          Food Products
-        </h1>
-        <div className="flex flex-row gap-4">
-          <Input
-            className="w-full md:max-w-80"
-            placeholder="Search for a product..."
-          />
-          <Button size={"icon"} variant={"outline"} className="w-16">
-            <Icons.search />
-          </Button>
-        </div>
-      </div>
-      <div className="grid grid-flow-row grid-cols-1 justify-items-center gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {food.map((item) => (
-          <FoodCard key={item.id} food={item} />
-        ))}
-      </div>
-    </section>
-  );
+  return <ProductCatalog user={session.user} food={food} />;
 }
---
+++ src/components/screens/product-catalog.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { type FoodProduct } from "@prisma/client";
+import { type User } from "next-auth";
+import FoodCard from "@/components/food-cart";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Icons } from "@/components/icons";
+import { useState } from "react";
+
+export default function ProductCatalog({
+  food,
+}: {
+  user: User;
+  food: ({
+    ingredients: {
+      id: number;
+      name: string;
+      image: string | null;
+    }[];
+  } & FoodProduct)[];
+}) {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchedFood, setSearchedFood] = useState(food.slice());
+
+  const update = (query: string) => {
+    if (query == "") setSearchedFood(food);
+
+    const similarityScore = (str: string, query: string) => {
+      const occurrences = str.split(query).length - 1;
+      return occurrences;
+    };
+
+    const foodCopy = food.slice();
+
+    setSearchedFood(
+      foodCopy.sort((a, b) => {
+        const scoreA = similarityScore(a.name, query);
+        const scoreB = similarityScore(b.name, query);
+        return scoreB - scoreA;
+      }),
+    );
+  };
+
+  return (
+    <section className="container grid items-center gap-6 pb-8 pt-3">
+      <div className="flex w-full flex-col items-center justify-between gap-2 sm:flex-row">
+        <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
+          Food Products
+        </h1>
+        <div className="flex flex-row gap-4">
+          <Input
+            className="w-full md:max-w-80"
+            placeholder="Search for a product..."
+            value={searchQuery}
+            onChange={(value) => {
+              setSearchQuery(value.target.value);
+              update(value.target.value);
+            }}
+          />
+          <Button size={"icon"} variant={"outline"} className="w-16">
+            <Icons.search />
+          </Button>
+        </div>
+      </div>
+      <div className="grid grid-flow-row grid-cols-1 justify-items-center gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {searchedFood.map((item) => (
+          <FoodCard key={item.id} food={item} />
+        ))}
+      </div>
+    </section>
+  );
+}
