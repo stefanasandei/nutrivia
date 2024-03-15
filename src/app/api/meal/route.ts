f4ccb3b29c4484552f4b1512ea019d2ae384f5c5
Asandei Stefan <asandei.stefanel@gmail.com>
2024-03-15 13:29:28 +0200

    add user allergies to the recipes generator

--- src/app/api/meal/route.ts
+++ src/app/api/meal/route.ts
@@ -1,21 +1,26 @@
 import OpenAI from 'openai';
 import { OpenAIStream, StreamingTextResponse } from 'ai';
 import { env } from '@/env';
-
-export const runtime = 'edge';
+import { api } from '@/trpc/server';
 
 const openai = new OpenAI({
     apiKey: 'ollama',
     baseURL: env.OLLAMA_API_ENDPOINT,
 });
 
-const system = `You are a chef's assistant. You have to provide interesting and healthy recipes upon request. You will format the recipes using markdown.
+const system = (allergies: string[]) => {
+    const allergiesText = allergies.length > 0 ? `The food can't contains the following ingredients: ${allergies.join(", ")}. ` : ``;
+
+    return `You are a chef's assistant. You have to provide interesting and healthy recipes upon request. You will format the recipes using markdown.
 The recipe will have three categories: "Brief description", "Ingredients" and "Instructions". Each of these sections will start with a "#", clearly marked as a new section.
-The user has entered the following preferences: 
-`;
+${allergiesText} The user has entered the following preferences: 
+`};
 
 export async function POST(req: Request) {
     const { prompt } = (await req.json()) as { prompt: string };
+
+    const user = await api.user.get.query();
+    const allergies = user?.allergies.map((a) => a.name) ?? [];
 
     const response = await openai.chat.completions.create({
         model: 'mistral',
@@ -23,7 +28,7 @@ export async function POST(req: Request) {
         temperature: 0.8,
         stream: true,
         messages: [
-            { role: "system", content: system },
+            { role: "system", content: system(allergies) },
             { role: "user", content: prompt }],
     });
 
